5060f94a9fbc990d17d63f70bbb09dbbbb7ae0be
Toleutai Bekzat <toleutaib@gmail.com>
2019-12-03 19:10:52 +0600

    Сделал загрузку как ProgressBar

---
+++ ProgressBar/ProgressBar/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace ProgressBar
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void StartLoad(object sender, RoutedEventArgs e)
+        {
+            DoubleAnimation animation = new DoubleAnimation();
+            animation.From = double.IsNaN(Canvas.GetLeft(loadRectangle)) ? 0 : Canvas.GetLeft(loadRectangle);
+            animation.To = ActualWidth;
+            animation.Duration = TimeSpan.FromSeconds(5);
+            animation.RepeatBehavior = RepeatBehavior.Forever;
+            loadRectangle.BeginAnimation(Canvas.LeftProperty, animation);
+        }
+    }
+}
